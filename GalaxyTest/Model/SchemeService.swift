cf72e2389347edfc4adf9dacccb812723dcc6b26
Alex <alex>
2020-06-10 04:01:49 -0500

    init commit

---
+++ GalaxyTest/Model/SchemeService.swift
@@ -0,0 +1,25 @@
+//
+//  SchemeService.swift
+//  GalaxyTest
+//
+//  Created by Alexander Stos on 6/10/20.
+//  Copyright © 2020 Alexander Stos. All rights reserved.
+//
+
+import Foundation
+
+class SchemeService {
+    // field declaration, etc
+}
+
+// MARK: SchemeSource
+extension SchemeService: SchemeSource {
+    
+    func getScheme(_ arg: [String]) -> Scheme? {
+        return nil
+    }
+    
+    func getSchemeOutside() -> Scheme? {
+        return nil
+    }
+}
