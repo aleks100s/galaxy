c60711000d145b9df4fc162ea1b8d91f672120d2
Alex <alex>
2020-06-10 03:23:34 -0500

    Initial Commit

---
+++ GalaxyTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GalaxyTest
+//
+//  Created by Alexander Stos on 6/10/20.
+//  Copyright © 2020 Alexander Stos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
